75d31dffb0e32b4f9931e63a7654407e11465c22
Dave Kalaluhi <dave.kalaluhi@gmail.com>
2026-07-28 12:28:56 -0400

    Working out the details of a gitcheck for multiple repositories. I don't always remember to commit and push before I sign off for the day.

--- zsh/90-experimental.zsh
+++ zsh/90-experimental.zsh
@@ -1,3 +1,67 @@
 cdb() {
     cd "$(git rev-parse --show-toplevel)"
 }
+
+gitcheck() {
+    local git_marker
+    local repo
+    local repo_name
+    local -i needs_staging=0
+    local -i needs_commit=0
+    local -i needs_push=0
+    local -i missing_upstream=0
+    local -i ahead_count=0
+
+    while IFS= read -r git_marker; do
+        needs_staging=0
+        needs_commit=0
+        needs_push=0
+        missing_upstream=0
+        ahead_count=0
+
+        git_marker=${git_marker%/}
+        repo=${git_marker:h}
+        repo_name=${repo:t}
+
+        if ! git -C "$repo" diff --quiet; then
+            needs_staging=1
+        fi
+
+        if [[ -n "$(git -C "$repo" ls-files --others --exclude-standard)" ]]; then
+            needs_staging=1
+        fi
+
+        if ! git -C "$repo" diff --cached --quiet --; then
+            needs_commit=1
+        fi
+
+        if [[ -n "$(git -C "$repo" remote)" ]]; then
+            if git -C "$repo" rev-parse --verify --quiet '@{upstream}' >/dev/null 2>&1; then
+                ahead_count=$(git -C "$repo" rev-list --count '@{upstream}..HEAD')
+
+                if (( ahead_count > 0 )); then
+                    needs_push=1
+                fi
+            else
+                missing_upstream=1
+            fi
+        fi
+
+        print
+        print -r -- "📁 $repo_name"
+        git -C "$repo" status --short --branch
+
+        if (( needs_staging )); then
+            print -r -- " -> Files needed to be staged"
+        fi
+
+        if (( needs_commit )); then
+            print -r -- " -> Files need to be committed"
+        fi
+        
+        if (( needs_push )); then
+            print -r -- " -> $ahead_count commit(s) need to be pushed"
+        fi
+        
+    done < <(fd -HI '^\.git$' ~/Projects)
+}
